85d404fb23c19b49934e104d6314c4dd76002255
Alexis Mejia <alemeji@gmail.com>
2021-11-10 13:14:04 -0500

    Adicionando anydesk en la instalacion

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common wget
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt-get update
@@ -10,6 +10,12 @@ sudo apt-get install -y docker-ce
 sudo usermod -aG docker ubuntu
 sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+sudo apt-get update
+sudo apt-get install -y anydesk
+
+#Imagenes
 sudo docker pull percona:5.7
 sudo docker alemeji/graphics:latest
 sudo docker rabbitmq:3.6.1
