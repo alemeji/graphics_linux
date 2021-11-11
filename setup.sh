36f186a7b2f028019233e826929b49bbddc0f91c
Alexis Mejia <alemeji@gmail.com>
2021-11-10 22:43:09 -0500

    mejora en script

--- setup.sh
+++ setup.sh
@@ -23,6 +23,6 @@ sudo docker pull phpmyadmin/phpmyadmin:4.8
 sudo docker pull portainer/portainer
 
 #Creacion del directorio de trabajo
-WORK_DIR = /proyectos/virtualenv
+WORK_DIR=/proyectos/virtualenv
 sudo mkdir -p $WORKDIR
-cp crontab.txt backup_db initdb.yml prod.yml $WORKDIR
+sudo cp crontab.txt backup_db initdb.yml prod.yml $WORKDIR
